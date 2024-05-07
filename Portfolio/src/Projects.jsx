13e02bf057b4c7a31b9cfe2937d6f31e56c57d9b
Marcin Bielinski <martibielinski@gmail.com>
2024-05-07 22:57:45 +0200

    Prawie dziala tekst w projektach, ustandaryzowany CSS

--- Portfolio/src/Projects.jsx
+++ Portfolio/src/Projects.jsx
@@ -5,28 +5,33 @@ import zsoPage from './assets/zso8Pagev2.mp4';
 function Projects()
 {
     const ProjectsItemTitle = (props) => {
-        return(<div className="projectsItemTitleContainer">
-            <h3 className="projectsItemTitle">{props.text}</h3>
-        </div>)
+        return(
+            <div className="projectsListItemTitleContainer">
+                <h3 className="projectsListItemTitle" style={{fontSize: props.size==="large"?'4.5vw':'2.5vw'}}>{props.text}</h3>
+            </div>);
+    }
+    const ProjectItem = (props) => {
+        return(
+            <div className="projectsListItemContainer">
+                <video className="projectsListItem" style={{height: props.size==="large"?'20vw':'9.65vw'}} src={props.src} type="video/mp4" autoPlay="autoplay" muted loop/>
+                <ProjectsItemTitle text={props.title} size={props.size}/>
+            </div>);
     }
     return(
     <section className="projects">
         <h2 className="projectsTitle">Moje projekty</h2>
-        <div className="projectsContainer">
-            <div className="projectsLeftColumn">
+        <div className="projectsListContainer">
+            <div className="projectsListLeftColumn">
                 <a href="http://17logdynia.pl/" target="_blank">
-                    <div className="projectsItemContainer">
-                        <video className="projectsMainItem" src={loPage} type="video/mp4" autoPlay="autoplay" muted loop/>
-                        <ProjectsItemTitle text="17 LO Gdynia"/>
-                    </div>
+                    <ProjectItem title="17 LO Gdynia" size="large" src={loPage}/>
                 </a>
             </div>
-            <div className="projectsRightColumn">
+            <div className="projectsListRightColumn">
                 <a href="http://sp37gdynia.pl/" target="_blank">
-                    <video className="projectsSecondaryItem projectsItem" src={spPage} type="video/mp4" autoPlay="autoplay" muted loop/>
+                    <ProjectItem title="SP 37 Gdynia" size="small" src={spPage}/>
                 </a>
                 <a href="http://zso8gdynia.pl/" target="_blank">
-                    <video className="projectsSecondaryItem projectsItem" src={zsoPage} type="video/mp4" autoPlay="autoplay" muted loop/>
+                    <ProjectItem title="ZSO 8 Gdynia" size="small" src={zsoPage}/>
                 </a>
             </div>
         </div>
