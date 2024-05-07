5eedbc66fed70dc618dc1fd4759d437ed41d4631
Marcin Bielinski <martibielinski@gmail.com>
2024-05-07 22:09:28 +0200

    Przed zamiana projects na elementy

--- Portfolio/src/Projects.jsx
+++ Portfolio/src/Projects.jsx
@@ -4,16 +4,30 @@ import spPage from './assets/sp37Pagev2.mp4';
 import zsoPage from './assets/zso8Pagev2.mp4';
 function Projects()
 {
+    const ProjectsItemTitle = (props) => {
+        return(<div className="projectsItemTitleContainer">
+            <h3 className="projectsItemTitle">{props.text}</h3>
+        </div>)
+    }
     return(
     <section className="projects">
         <h2 className="projectsTitle">Moje projekty</h2>
         <div className="projectsContainer">
             <div className="projectsLeftColumn">
-                <video className="projectsMainItem" src={loPage} type="video/mp4" autoplay="autoplay" muted loop></video>
+                <a href="http://17logdynia.pl/" target="_blank">
+                    <div className="projectsItemContainer">
+                        <video className="projectsMainItem" src={loPage} type="video/mp4" autoPlay="autoplay" muted loop/>
+                        <ProjectsItemTitle text="17 LO Gdynia"/>
+                    </div>
+                </a>
             </div>
             <div className="projectsRightColumn">
-                <video className="projectsSecondaryItem" src={spPage} type="video/mp4" autoplay="autoplay" muted loop></video>
-                <video className="projectsSecondaryItem" src={zsoPage} type="video/mp4" autoplay="autoplay" muted loop></video>
+                <a href="http://sp37gdynia.pl/" target="_blank">
+                    <video className="projectsSecondaryItem projectsItem" src={spPage} type="video/mp4" autoPlay="autoplay" muted loop/>
+                </a>
+                <a href="http://zso8gdynia.pl/" target="_blank">
+                    <video className="projectsSecondaryItem projectsItem" src={zsoPage} type="video/mp4" autoPlay="autoplay" muted loop/>
+                </a>
             </div>
         </div>
     </section>);
